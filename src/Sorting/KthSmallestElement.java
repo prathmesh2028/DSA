e7c5ba30fa2b10e06c2d74d748974d8fd53071a8
PRATHMESH BADHE <wdwwc@sinhgad.edu>
2025-12-04 07:31:22 +0530

    selection

--- src/Sorting/KthSmallestElement.java
+++ src/Sorting/KthSmallestElement.java
@@ -1,26 +1,25 @@
 package Sorting;
 
-public class SelectionSort {
+public class KthSmallestElement {
     public static void main(String[] args) {
-        int[] arr = {5,4,2,7,8,1,9,0,3,2};
+        int[] arr = {8,4,1,5,9,8,2,3,6,7};
+        int k = 3;
         int n = arr.length;
 
-        for(int i = 0 ; i < n -1 ; i++){
+        for(int i = 0 ; i < k ; i++){
             int min = Integer.MAX_VALUE;
             int mindex = -1;
-            for(int j = i ; j < n  ; j++){
+            for(int j = i ; j < n ; j++){
                 if(arr[j] < min){
                     min = arr[j];
                     mindex = j;
                 }
             }
-
-            // swap
             int temp = arr[i];
             arr[i] = arr[mindex];
             arr[mindex] = temp;
         }
-
         for(int i : arr) System.out.print(i + " ");
+        System.out.println(arr[k-1]);
     }
 }
