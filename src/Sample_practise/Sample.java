167298fe149922b37e357cbb20990735b51f9515
PRATHMESH BADHE <wdwwc@sinhgad.edu>
2025-12-04 08:53:04 +0530

    selection

--- src/Sample_practise/Sample.java
+++ src/Sample_practise/Sample.java
@@ -4,10 +4,22 @@ import java.util.Scanner;
 
 public class Sample {
     public static void main(String[] args) {
-        int n = 5;
-        for(int i = 2 ; i <= n ; i*=i){
-            System.out.println("GFG");
+
+
+        // code for the bubble sort
+
+        int[] arr = {5,2,8,-2,0,1,3,6,5,7};
+        int n = arr.length;
+        for(int i = 0 ; i < n-2 ; i++){
+            for(int j = 0 ; j < n - 1 - i ; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
         }
+        for(int i : arr) System.out.print(i + "  ");
 
 
     }
