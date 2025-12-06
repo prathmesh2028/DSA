1c279287c446b93d5b5e9de1c0e5d532569ac89e
PRATHMESH BADHE <wdwwc@sinhgad.edu>
2025-12-06 21:31:43 +0530

    GFG question

---
+++ src/Practise/GreaterOrNotorEqual.java
@@ -0,0 +1,11 @@
+package Practise;
+
+public class GreaterOrNotorEqual {
+    // User function Template for Java
+    class Solution {
+        public static String compareNM(int n, int m) {
+            // code here
+            return (n < m) ? "lesser" : (n > m) ? "greater" : "equal";
+        }
+    }
+}
