800ab409a14a82706af1dfaf2713be908d49376a
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2025-12-12 00:19:52 +0530

    2D  arrays quesions

--- src/TwoDArrays/Printing2DArrayColumnWise.java
+++ src/TwoDArrays/Printing2DArrayColumnWise.java
@@ -2,9 +2,10 @@ package TwoDArrays;
 
 public class Printing2DArrayColumnWise {
     public static void main(String[] args) {
-        int [][]arr = {{1,2,3,4},{5,6,7,8},{9,1,2,0},{3,1,5,6}};
-        for(int i = 0 ; i < arr.length ; i++){
-            for(int j = 0 ; j < arr[0].length ; j++){
+//        int [][]arr = {{1,2,3,4},{5,6,7,8},{9,1,2,0},{3,1,5,6}};
+        int [][] arr = {{6,0,2,7,6} , {1,3,7,2,2},{9,9,4,5,2}};
+        for(int i = 0 ; i < arr[0].length ; i++){
+            for(int j = 0 ; j < arr.length ; j++){
                 System.out.print(arr[j][i] + " ");
             }
             System.out.println();
