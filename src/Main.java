a5166ea11ca2a229f5e6b377601ad7e559cc803a
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-01-07 14:57:40 +0530

    2D array

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,21 @@
 public class Main{
     public static void main(String[] args) {
-        int[] arr = {1 , 0 , 0 , 1 , 1 , 1 , 0 , 0 , 1 , 1 , 0};
-        int n = arr.length;
-
-//        int os = 0;
-//        int ones = 0;
-//        for(int i = 0 ; i < n ; i++){
-//            if(arr[i] == 0) os ++;
-//            else ones ++;
-//        }
-//        for(int i = 0 ; i < os ; i++){
-//            arr[i] = 0;
-//        }
-//        for(int i = os ; i < n ; i++){
-//            arr[i] = 1;
-//        }
-//        printarr(arr);
-
-        // two pointer approach
-        int s = 0;
-        int e = n - 1;
-        while(s <= e){
-            if(arr[s] == 1 && arr[e] == 0){
-                int temp = arr[s];
-                arr[s] = arr[e];
-                arr[e] = temp;
+        int[] nums = {437,315,322,431,686,264,442};
+        int n = nums.length;
+        int count = 0;
+        int rc = 0;
+        for(int i= 0 ; i < n ; i++){
+            count = 0;
+            int temp = nums[i];
+            while(temp > 0){
+                int digit = temp % 10;
+                temp /= 10;
+                count++;
             }
-            s++;
-            e--;
+            if(count % 2 == 0){
+                rc++;
+            }
         }
-        printarr(arr);
-
-
-
-
-
-
-
-    }
-    public static void printarr(int[] arr){
-        for(int i = 0 ; i < arr.length ; i++){
-            System.out.print(arr[i] + "  ");
-        }
-    }
-    public static void revarr(int[] arr , int a , int b){
-        int i = a;
-        int j = b;
-        while(i <= j){
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
-        }
+        System.out.println(rc);
     }
 }
--- src/Sample_practise/Sample.java
+++ src/Sample_practise/Sample.java
@@ -3,16 +3,24 @@ package Sample_practise;
 import java.util.*;
 class Sample{
     public static void main(String[] args) {
-        // wave array problem
-        int[] arr = {1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 };
-        int d = 2;
-
+        int [][] arr = {{1,2,3} , {4,5,6}};
+        int a = arr.length;
+        int b = arr[0].length;
+        printArr(arr);
+        int[][] newArr = new int[b][a];
+        for(int i = 0 ; i < newArr.length ; i++){
+            for(int j = 0 ; j < newArr[0].length ; j++){
+                newArr[i][j] = arr[j][i];
+            }
+        }
+        printArr(newArr);
     }
-    public static void revArr(int[] arr , int a , int b){
-        int i = a;
-        int j = b;
-        while(i < j){
-//            int temp =
+    public static void printArr(int[][] arr){
+        for(int i = 0 ; i < arr.length ; i++){
+            for(int j = 0 ; j < arr[0].length ; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
