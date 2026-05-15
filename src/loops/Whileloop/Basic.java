0edd6f6f0ceadf0aa6e9b369062aa8839040dcb8
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-05-15 13:49:03 +0530

    basic of while loop

---
+++ src/loops/Whileloop/Basic.java
@@ -0,0 +1,12 @@
+package loops.Whileloop;
+
+public class Basic {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i <= 100)
+        {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
