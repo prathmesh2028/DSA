6d41c8c154604d0402131d892dfd54320f08d98f
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-05-15 08:36:23 +0530

    for loop

--- src/loops/Forloop/OddNumbersDivisibleBy31To100.java
+++ src/loops/Forloop/OddNumbersDivisibleBy31To100.java
@@ -11,5 +11,15 @@ public class OddNumbersDivisibleBy31To100 {
                 System.out.print(i + " ");
             }
         }
+
+        System.out.println();
+        // optimized
+        for(int i = 1 ; i <= 100 ; i += 2)
+        {
+            if((i % 3 == 0))
+            {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
