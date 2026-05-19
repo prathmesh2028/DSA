92b3baf0f335574c95c2a04d65c1ad63e7e7255e
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-05-19 10:39:53 +0530

    sample

--- src/Sample_practise/Sample.java
+++ src/Sample_practise/Sample.java
@@ -1,38 +1,20 @@
 package Sample_practise;
 
-import java.util.*;
+//import java.util.*;
 class Sample{
     public static void main(String[] args) {
-       // bridge pattern here :)
-        int n = 5;
-        int a = 1;
-        for(int i = 1 ; i <= (2*n-1) ; i++)
+       // number spiral pattern here :)
+        int n = 7;
+        for(int i = 1 ; i <= 2*n-1 ; i++)
         {
-            System.out.print("* ");
-        }
-        System.out.println();
-        for(int i = 1 ; i <= (n-1) ; i++)
-        {
-            for(int j = 1 ; j <= (n-i) ; j++)
+            for(int j = 1 ; j <= 2*n-1 ; j++)
             {
-                System.out.print("* ");
+                int a = i , b = j;
+                if(a > n) a = 2*n-i;
+                if(b > n) b = 2*n-j;
+                System.out.print(Math.min(a,b) + " ");
             }
-            for(int j = 1 ; j <= a ; j++)
-            {
-                System.out.print("  ");
-            }
-            for(int j = 1 ; j <= (n-i) ; j++)
-            {
-                System.out.print("* ");
-            }
-            a += 2;
             System.out.println();
         }
     }
-
-
-
-
-
-
 }
