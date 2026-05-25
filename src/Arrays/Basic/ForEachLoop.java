244557944cbc513dd2f15c4bfb3b51c50b70fed5
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-05-25 22:48:17 +0530

    for each loop

---
+++ src/Arrays/Basic/ForEachLoop.java
@@ -0,0 +1,9 @@
+package Arrays.Basic;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int[] arr = {5, 8, 2, 6, 9};
+        for (int element : arr) System.out.print(element + " ");
+
+    }
+}
