c8449c33097e3c7f4b0b150249a674157544f779
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-05-27 08:23:31 +0530

    merging two sorted arrays in reverse order in one single array

--- src/Arrays/MergeTwoSortedArraysInReverseWay.java
+++ src/Arrays/MergeTwoSortedArraysInReverseWay.java
@@ -2,6 +2,49 @@ package Arrays;
 
 public class MergeTwoSortedArraysInReverseWay {
     public static void main(String[] args) {
+        int[] a = {1,3,5,7,9,29};
+        int[] b = {2,4,6,8,10,90};
+        int[] c = new int[a.length + b.length];
 
+        int i = a.length-1 , j = b.length-1 , k = c.length-1;
+        while(i >= 0 && j >= 0)
+        {
+            if(a[i] < b[j])
+            {
+                c[k] = b[j];
+                k--;
+                j--;
+            }
+            else if(a[i] > b[j])
+            {
+                c[k] = a[i];
+                i--;
+                k--;
+            }
+            else if(a[i] == b[j])
+            {
+                c[k] = a[i];
+                k--;
+                i--;
+                c[k] = b[j];
+                k--;
+                j--;
+            }
+        }
+
+        while(i >= 0)
+        {
+            c[k] = a[i];
+            k--;
+            i--;
+        }
+        while(j >= 0)
+        {
+            c[k] = b[j];
+            k--;
+            j--;
+        }
+
+        for(int f:c) System.out.print(f + " ");
     }
 }
