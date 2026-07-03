de57090aea53b61994f265127c724f3e40d19363
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-07-03 20:08:34 +0530

    merging two sorted arrays in reverse order in one single array

--- src/Arrays/Sort0s1sAnd2s.java
+++ src/Arrays/Sort0s1sAnd2s.java
@@ -1,4 +1,29 @@
 package Arrays;
-
+import java.util.*;
 public class Sort0s1sAnd2s {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 0, 1, 2, 1, 0, 0, 0, 1};
+
+        // merge sort brute force
+//        Arrays.sort(arr);
+        for(int i:arr) System.out.print(i + " ");
+
+        int n0 = 0 , n1 = 0 , n2= 0;
+        for(int i = 0 ; i < arr.length ; i++)
+        {
+            if(arr[i] == 0) n0++;
+            if(arr[i] == 1) n1++;
+            if(arr[i] == 2) n2++;
+        }
+
+        for(int i = 0 ; i < n0 ; i++)
+        {
+            arr[i] = 0;
+        }
+        for(int i = n0 ; i < n0+n1 ; i++) arr[i] = 1;
+        for(int i = n0+n1 ; i < arr.length ; i++) arr[i] = 2;
+        System.out.println();
+        for(int i:arr) System.out.print(i + " ");
+
+    }
 }
---
+++ src/Arrays/Sort0s1sAnd2sDutchFlagAlgorithm.java
@@ -0,0 +1,42 @@
+package Arrays;
+
+public class Sort0s1sAnd2sDutchFlagAlgorithm {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 0, 1, 2, 1, 0, 0, 0, 1};
+
+        System.out.println("Before sorting.....");
+        for(int i :arr) System.out.print(i + " ");
+        System.out.println();
+
+
+        int low = 0 , mid = 0 , high = arr.length - 1;
+
+        while(mid <= high)
+        {
+            if(arr[mid] == 0)
+            {
+                int temp = arr[mid];
+                arr[mid] = arr[low];
+                arr[low] = temp;
+                low++;
+                mid++;
+            }
+            else if(arr[mid] == 1)
+            {
+                mid++;
+            }
+            else if(arr[mid] == 2)
+            {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+
+
+        System.out.println("After sorting.......");
+        for(int i : arr) System.out.print(i + " ");
+
+    }
+}
