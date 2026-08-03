b13355a96005c5587f7762ede505a658ca54a66e
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-08-03 16:31:40 +0530

    for each loop

---
+++ src/Arrayss/Array_Revised/ForEachLoop.java
@@ -0,0 +1,11 @@
+package Arrayss.Array_Revised;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        for(int ele: arr)
+        {
+            System.out.print(ele + " ");
+        }
+    }
+}
