297de5a298fd65e1029dc936fdad209b970d41b2
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-08-03 16:37:53 +0530

    reverse array function add

--- src/Arrayss/Array_Revised/ReverseArray.java
+++ src/Arrayss/Array_Revised/ReverseArray.java
@@ -17,4 +17,16 @@ public class ReverseArray {
         }
         System.out.println(Arrays.toString(arr));
     }
+    public static void revArr(int[] arr , int i , int j)
+    {
+        while(i < j)
+        {
+            int temp  = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+
+    }
 }
