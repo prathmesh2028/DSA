cea7f9f584aaeb9e1688fe7a5f171da6d1c43fd2
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-08-10 23:09:15 +0530

    TransposeOfMatrix

--- src/Arrayss/Array_Revised/Two_D_Array/TransposeOfMatrix.java
+++ src/Arrayss/Array_Revised/Two_D_Array/TransposeOfMatrix.java
@@ -16,7 +16,7 @@ public class TransposeOfMatrix {
 
         for(int i = 0 ; i < arr.length ; i++)
         {
-            for(int j = 0 ; j <= i ; j++)
+            for(int j = 0 ; j < i ; j++)
             {
                 int temp = arr[i][j];
                 arr[i][j] = arr[j][i];
