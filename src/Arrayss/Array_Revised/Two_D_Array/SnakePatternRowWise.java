54f89ed5e8857df50e0436310e59d71ed4bc1fdb
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-08-10 22:37:47 +0530

    SnakePattern

--- src/Arrayss/Array_Revised/Two_D_Array/SnakePatternRowWise.java
+++ src/Arrayss/Array_Revised/Two_D_Array/SnakePatternRowWise.java
@@ -1,6 +1,6 @@
 package Arrayss.Array_Revised.Two_D_Array;
 
-public class SnakePattern {
+public class SnakePatternRowWise {
     public static void main(String[] args) {
         int[][] arr = {{2,8,3,4,7} , {7,2,1,6,3} , {5,5,4,1,4} , {3,1,8,2,6}};
         for(int i = 0 ; i < arr.length ; i++)
