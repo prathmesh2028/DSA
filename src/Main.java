d694900824ebce06dd49ae243513b865a508c3e1
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-08-10 17:33:12 +0530

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main{
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {
-        int[] x = {6,19,7,69,7,171,8};
+        int[] x = {6,1,7,6,7};
         // indexing
     }
 }
