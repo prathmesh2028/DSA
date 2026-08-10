f3fe83bb0160fb6d9d541daa91495047790aeb43
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-08-10 22:46:13 +0530

    TransposeOfMatrix

---
+++ src/Arrayss/Array_Revised/Two_D_Array/TransposeOfMatrix.java
@@ -0,0 +1,36 @@
+package Arrayss.Array_Revised.Two_D_Array;
+
+public class TransposeOfMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {{2,8,3,4} , {7,2,1,6} , {5,5,4,1} , {3,1,8,2}};
+        for(int i = 0 ; i < arr.length ; i++)
+        {
+            for(int j = 0 ; j < arr[0].length ; j++)
+            {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for(int i = 0 ; i < arr.length ; i++)
+        {
+            for(int j = 0 ; j <= i ; j++)
+            {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+
+        for(int i = 0 ; i < arr.length ; i++)
+        {
+            for(int j = 0 ; j < arr[0].length ; j++)
+            {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
