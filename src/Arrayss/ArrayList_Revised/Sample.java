08289eebbd93eb46a9b0ecf9cbb78fef2ff25e29
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-08-10 23:17:17 +0530

    Sample

---
+++ src/Arrayss/ArrayList_Revised/Sample.java
@@ -0,0 +1,12 @@
+package Arrayss.ArrayList_Revised;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sample {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>>list = new ArrayList<>();
+        list.add(new ArrayList<>(List.of(1,2,3)));
+
+    }
+}
