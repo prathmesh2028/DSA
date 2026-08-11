852adc3d7c0fc47bc62a59ca2a949c1e299ed4f9
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-08-11 21:36:16 +0530

    2D arrays

--- src/Arrayss/ArrayList_Revised/Sample.java
+++ src/Arrayss/ArrayList_Revised/Sample.java
@@ -5,8 +5,32 @@ import java.util.List;
 
 public class Sample {
     public static void main(String[] args) {
-        ArrayList<ArrayList<Integer>>list = new ArrayList<>();
-        list.add(new ArrayList<>(List.of(1,2,3)));
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(3); a.add(13) ; a.add(36) ; a.add(36);
+        ArrayList<Integer> b = new ArrayList<>();
+        b.add(3); b.add(13) ; b.add(36) ; b.add(36);
+        ArrayList<Integer> c = new ArrayList<>();
+        c.add(3); c.add(13) ; c.add(36) ; c.add(36);
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+        arr.add(a); arr.add(b); arr.add(c);
+
+//        System.out.println(arr);
+        for(int i = 0 ; i < arr.size() ; i++)
+        {
+            for(int j  = 0 ; j < arr.get(i).size() ; j++)
+            {
+                System.out.print(arr.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+
+
+        for(ArrayList<Integer> list:arr)
+        {
+            for(int ele:list) System.out.print(ele + " ");
+            System.out.println();
+        }
+
 
     }
 }
