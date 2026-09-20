fa506bb1a997315bdf66fcf550eb5f4bc2e16a57
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-09-20 20:14:01 +0530

    bubble sort optimized

--- src/Sorting/Sorting_Revised/BubbleSort_Better.java
+++ src/Sorting/Sorting_Revised/BubbleSort_Better.java
@@ -30,5 +30,25 @@ public class BubbleSort_Better {
             }
         }
         System.out.println(Arrays.toString(arr));
+
+
+
+        // another approach
+
+        for(int i = 0 ; i < arr.length ; i++)
+        {
+            int swaps = 0;
+            for(int j = 0 ; j < arr.length - i -1 ; j++)
+            {
+                if(arr[j] > arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+                swaps++;
+            }
+            if(swaps==0) break;
+        }
     }
 }
